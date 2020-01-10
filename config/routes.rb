e48dc7104589bc3f5292bd8a0341bf7ccc772c56
codershona <falgunimst95@gmail.com>
2020-01-10 22:55:35 +0100

    navbar created and added in bootstrap , styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	get 'about' => 'pages#about', as: :about 
+	get 'blog' => 'pages#blog', as: :blog 
 	root to: 'posts#index'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
