500ecb51c1d15ac73334e8cd1a40045d67eb9624
codershona <falgunimst95@gmail.com>
2020-01-11 20:17:04 +0100

    routes files errors fixes and my posts buttons added in navbar

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,3 +1,7 @@
 class BlogController < ActionController::Base
 	protect_from_forgery with: :exception
+
+	def blog
+
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,19 @@ Rails.application.routes.draw do
 
 	root to: 'blog/posts#index'
 
+	namespace :author do
+    resources :posts
+     end
+
 	scope module: 'blog' do
 	get 'about' => 'pages#about', as: :about 
 	get 'blog' => 'pages#blog', as: :blog 
 	get 'contact' => 'pages#contact', as: :contact
-		resources :posts
+	get 'posts' => 'posts#index', as: :posts
+	get 'posts/:id' => 'posts#show', as: :post
+	
+   # 'posts/jimbob' => params[:favourite_cat] == 'jimbob'
+
 	end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
