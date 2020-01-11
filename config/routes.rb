55e790b6fdee73d5a5daa8cbdb8e83b18ac45922
codershona <falgunimst95@gmail.com>
2020-01-11 18:55:35 +0100

    styling the posts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	get 'about' => 'pages#about', as: :about 
 	get 'blog' => 'pages#blog', as: :blog 
+	get 'contact' => 'pages#contact', as: :contact
 	root to: 'posts#index'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
