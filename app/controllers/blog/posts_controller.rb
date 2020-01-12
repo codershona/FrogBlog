11165e42226d8b92b66a7071812be7547a5128a6
codershona <falgunimst95@gmail.com>
2020-01-12 22:46:11 +0100

    finally my blog/index page errors fixed and some plugins added in my edit posts page

--- app/controllers/blog/posts_controller.rb
+++ app/controllers/blog/posts_controller.rb
@@ -6,13 +6,13 @@
     # GET /posts
     # GET /posts.json
     def index
-      @posts = current_author.posts.most_recent
+     @posts = Post.most_recent 
     end
 
     # GET /posts/1
     # GET /posts/1.json
     def show
-       @post = current_author.posts.friendly.find(params[:id])
+        @post = Post.friendly.find(params[:id])
     end
 
 
