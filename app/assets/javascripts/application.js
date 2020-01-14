8d06ac1f473793463b205de3948ba909f9a5780e
codershona <falgunimst95@gmail.com>
2020-01-14 18:40:48 +0100

    styling the edit posts pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,7 @@
 document.addEventListener("turbolinks:load", function() {
   tinymce.remove();
   tinymce.init({ 
+  	height: '495', 
   	selector:'textarea#post_body',
   	 plugins: "codesample image media link code ",
   toolbar: "undo redo | styleselect | bold italic link | codesample image media | code "
